853b573e074c26e46fa642ba21d2a352b73e058d
Nick Tehrany <nicktehrany1@gmail.com>
2023-08-29 20:45:18 +0000

    initial plotting script

---
+++ loaded-reset/plot.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python3
+
+import os
+import glob
+import json
+
+def parse_fio_data(data_path, data):
+    if not os.path.exists(f'{data_path}') or \
+            os.listdir(f'{data_path}') == []:
+        print(f"No data in {data_path}")
+        return 0
+
+    for file in glob.glob(f'{data_path}/*'):
+        with open(file, 'r') as f:
+            for index, line in enumerate(f, 1):
+                # Removing all fio logs in json file by finding first {
+                if line.split()[0] == "{":
+                    rows = f.readlines()
+                    with open(os.path.join(os.getcwd(), "temp.json"), 'w+') as temp:
+                        temp.write(line)
+                        temp.writelines(rows)
+                    break
+        with open(os.path.join(os.getcwd(), "temp.json"), 'r') as temp:
+            data[file] = dict()
+            data[file] = json.load(temp)
+            os.remove(os.path.join(os.getcwd(), "temp.json"))
+
+    return 1
+
+if __name__ == "__main__":
+    file_path = '/'.join(os.path.abspath(__file__).split('/')[:-1])
+
+    data = dict()
+    parse_fio_data(file_path, data)
+    print(data)
