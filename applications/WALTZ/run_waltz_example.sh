4557b08338ed2c03379cb42e72b886c07aff6807
Krijn Doekemeijer <krijn.doekemeijer@gmail.com>
2023-10-12 12:56:42 +0200

    Add WALTZ

--- applications/WALTZ/run_waltz_example.sh
+++ applications/WALTZ/run_waltz_example.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sudo LD_LIBRARY_PATH="/home/user/src/WALTZ/plugin/spdk/build/lib/:/home/user/src/WALTZ/plugin/spdk/dpdk/build/lib/" ./db_bench --benchmarks=fillrandom --waltz_pcie_addr=0000:00:06.0 --fs_uri=zenfs://dev:ultra --waltz_test_type=0
+
+sudo LD_LIBRARY_PATH="/home/user/src/WALTZ/plugin/spdk/build/lib/:/home/user/src/WALTZ/plugin/spdk/dpdk/build/lib/" ./zenfs list --zns_pci=0000:00:06.0 --zbd=Ultra --path=rocksdbtest/dbbench
