7eba8b6bbaa9a861e2f3c70c987301680569f8d9
Nick Tehrany <nicktehrany1@gmail.com>
2023-11-26 22:06:46 +0000

    Add basic parsing of interference results + some plotting

--- zinc-configuration/plot.py
+++ zinc-configuration/plot.py
@@ -6,6 +6,7 @@ import glob
 import json
 import math
 import matplotlib.pyplot as plt
+from pathlib import Path
 import numpy as np
 import matplotlib.patches as mpatches
 
@@ -13,6 +14,10 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from interference_model.quantification import get_interference_rms
+
+# TODO: retrieve these values from ZenFS tracing for final calculation
+WRITE_INTERFERENCE_GAMMA = 0.5
+RESET_INTERFERENCE_DELTA = 0.5
 
 plt.rc('font', size=12)          # controls default text sizes
 plt.rc('axes', titlesize=12)     # fontsize of the axes title
@@ -26,7 +31,7 @@ def parse_fio_data(data_path, data):
             os.listdir(f'{data_path}') == []:
         print(f"No data in {data_path}")
         return 0
-
+    
     for file in glob.glob(f'{data_path}/*'):
         if "bw" in file:
             continue
@@ -40,81 +45,135 @@ def parse_fio_data(data_path, data):
                         temp.writelines(rows)
                     break
 
-        with open(os.path.join(os.getcwd(), "temp.json"), 'r') as temp:
-            data[file] = dict()
-            data[file] = json.load(temp)
-            os.remove(os.path.join(os.getcwd(), "temp.json"))
+        if Path(os.path.join(os.getcwd(), "temp.json")).exists(): 
+            with open(os.path.join(os.getcwd(), "temp.json"), 'r') as temp:
+                data[file] = dict()
+                data[file] = json.load(temp)
+                os.remove(os.path.join(os.getcwd(), "temp.json"))
 
     return 1
+
+def init_baseline(baseline):
+    val = baseline[0]
+
+    i = 0
+    for value in baseline:
+        baseline[i] = val
+        i += 1
+
+def parse_write_baseline(write_baseline_iops, write_baseline_lat):
+     for conf_key, conf_value in data.items():
+
+        for key, value in conf_value.items():
+            if conf_key == "baseline":
+                if "reset_baseline" in key:
+                    continue
+                elif "bw" in key:
+                    continue
+                else:
+                    head, tail = os.path.split(key)
+                    numjobs = int(re.search(r'\d+', tail).group())
+                    write_baseline_iops[numjobs - 1] =  value["jobs"][0]["finish"]["iops_mean"]/1000  # this is a bug in fio it being under the "finish" instead of "write"
+                    write_baseline_lat[numjobs - 1] = value["jobs"][0]["finish"]["lat_ns"]["percentile"]["95.000000"]/1000
+            else:
+                continue
+
+def parse_reset_baseline(reset_baseline_iops, reset_baseline_write):
+     for conf_key, conf_value in data.items():
+
+        for key, value in conf_value.items():
+            if conf_key == "baseline":
+                if "reset_baseline" in key:
+                    reset_baseline_iops[0] = value["jobs"][1]["ZNS Reset"]["iops_mean"]
+                    reset_baseline_lat[0] = value["jobs"][1]["ZNS Reset"]["lat_ns"]["percentile"]["95.000000"]/1000 # TODO: change this to clat after rerunning
+                    init_baseline(reset_baseline_iops)
+                    init_baseline(reset_baseline_lat)
+
+                    return
+                else:
+                    continue
+            else:
+                continue
 
 if __name__ == "__main__":
     file_path = '/'.join(os.path.abspath(__file__).split('/')[:-1])
 
     data = dict()
-    parse_fio_data(f"{file_path}/data", data)
+    for dir in glob.glob(f'*'):
+        dir = dir.split('/')[-1]
+        if dir == "data":
+            data["baseline"] = dict()
+            parse_fio_data(f"{file_path}/{dir}", data["baseline"])
+        elif "data" in dir:
+            config = re.findall(r'\d+', dir)
+            config_string = f"reset_lat_{config[0]}-write_ratio_{config[1]}"
+            data[config_string] = dict()
+            parse_fio_data(f"{file_path}/{dir}", data[config_string])
+
+    os.makedirs(f"{file_path}/figures", exist_ok=True)
 
     queue_depths = np.arange(1, 8)
+    lowest_interference = (None, None)
 
-    write100 = [None] * len(queue_depths)
-    write100_iops = [None] * len(queue_depths)
-    write99 = [None] * len(queue_depths)
-    write99_iops = [None] * len(queue_depths)
-    write95 = [None] * len(queue_depths)
-    write95_iops = [None] * len(queue_depths)
-    write90 = [None] * len(queue_depths)
-    write90_iops = [None] * len(queue_depths)
-    write75 = [None] * len(queue_depths)
-    write75_iops = [None] * len(queue_depths)
-    write50 = [None] * len(queue_depths)
-    write50_iops = [None] * len(queue_depths)
+    reset_baseline_iops = [None] * len(queue_depths)
+    reset_baseline_lat = [None] * len(queue_depths)
+    write_baseline_iops = [None] * len(queue_depths)
+    write_baseline_lat = [None] * len(queue_depths)
 
-    for key, value in data.items():
-        if "bw" in key:
-            continue
-        numjobs = int(re.search(r'\d+', key).group())
-        if numjobs > 7:
-            continue # We skip 14 as it turns out we were bottlenecked by not enough cpu cores, giving inaccurate results
-        else:
-            x = numjobs - 1
-        if 'wflow_100' in key:
-            write100[x] = value["jobs"][0]["write"]["lat_ns"]["percentile"]["95.000000"]/1000
-            write100_iops[x] = value["jobs"][0]["write"]["iops_mean"]/1000
-        elif 'wflow_99' in key:
-            write99[x] = value["jobs"][1]["write"]["lat_ns"]["percentile"]["95.000000"]/1000
-            write99_iops[x] = value["jobs"][1]["write"]["iops_mean"]/1000
-        elif 'wflow_95' in key:
-            write95[x] = value["jobs"][1]["write"]["lat_ns"]["percentile"]["95.000000"]/1000
-            write95_iops[x] = value["jobs"][1]["write"]["iops_mean"]/1000
-        elif 'wflow_90' in key:
-            write90[x] = value["jobs"][1]["write"]["lat_ns"]["percentile"]["95.000000"]/1000
-            write90_iops[x] = value["jobs"][1]["write"]["iops_mean"]/1000
-        elif 'wflow_75' in key:
-            write75[x] = value["jobs"][1]["write"]["lat_ns"]["percentile"]["95.000000"]/1000
-            write75_iops[x] = value["jobs"][1]["write"]["iops_mean"]/1000
-        elif 'wflow_50' in key:
-            write50[x] = value["jobs"][1]["write"]["lat_ns"]["percentile"]["95.000000"]/1000
-            write50_iops[x] = value["jobs"][1]["write"]["iops_mean"]/1000
+    parse_write_baseline(write_baseline_iops, write_baseline_lat)
+    parse_reset_baseline(reset_baseline_iops, reset_baseline_lat)
 
-    print(f"Interference RMS 0% - 50% {get_interference_rms(write100_iops, write50_iops, write100, write50)}")
+    for conf_key, conf_value in data.items():
+        write_iops = [None] * len(queue_depths)
+        write_lat = [None] * len(queue_depths)
+        reset_iops = [None] * len(queue_depths)
+        reset_lat = [None] * len(queue_depths)
 
-    fig, ax = plt.subplots()
+        for key, value in conf_value.items():
+            if conf_key == "baseline":
+               continue
+            else:
+                head, tail = os.path.split(key)
+                numjobs = int(re.search(r'\d+', tail).group())
+                x = numjobs - 1
+                write_iops[x] = value["jobs"][2]["finish"]["iops_mean"]/1000
+                write_lat[x] = value["jobs"][2]["finish"]["clat_ns"]["percentile"]["95.000000"]/1000
+                reset_iops[x] = value["jobs"][1]["ZNS Reset"]["iops_mean"]
+                reset_lat[x] = value["jobs"][1]["ZNS Reset"]["clat_ns"]["percentile"]["95.000000"]/1000
 
-    ax.plot(write100_iops, write100, markersize = 4, marker = '>', label="  0% reset")
-    ax.plot(write99_iops, write99, markersize = 4, marker = 'x', label="  1% reset")
-    ax.plot(write95_iops, write95, markersize = 4, marker = 'o', label="  5% reset")
-    ax.plot(write90_iops, write90, markersize = 4, marker = '<', label=" 10% reset")
-    ax.plot(write75_iops, write75, markersize = 4, marker = '^',  label=" 25% reset")
-    ax.plot(write50_iops, write50, markersize = 4, marker = '*', label=" 50% reset")
+        # While debugging skip all that are ongoing and don't have all data
+        if not None in write_iops:
+            write_interference = get_interference_rms(write_baseline_iops, write_iops, write_baseline_lat, write_lat)
+            reset_interference = get_interference_rms(reset_baseline_iops, reset_iops, reset_baseline_lat, reset_lat)
 
-    fig.tight_layout()
-    ax.grid(which='major', linestyle='dashed', linewidth='1')
-    ax.set_axisbelow(True)
-    # ax.legend(loc='best', handles=handles)
-    ax.legend(loc='best')
-    ax.set_ylim(bottom=0, top=140)
-    ax.set_xlim(left=0)
-    ax.set_ylabel("p95 write Latency (usec)")
-    ax.set_xlabel("Total IOPS (x1000)")
-    plt.savefig(f"{file_path}/loaded_write_latency.pdf", bbox_inches="tight")
-    plt.savefig(f"{file_path}/loaded_write_latency.png", bbox_inches="tight")
-    plt.clf()
+            # print(f"Config {conf_key} WRITE Interference RMS {write_interference}")
+            # print(f"Config {conf_key} RESET Interference RMS {reset_interference}")
+
+            interference = WRITE_INTERFERENCE_GAMMA * write_interference + RESET_INTERFERENCE_DELTA * reset_interference
+            print(f"Config {conf_key} Interference RMS {interference}")
+
+            inter = lowest_interference[1]
+            if inter == None:
+                lowest_interference = (conf_key, interference)
+            elif interference < inter:
+                    lowest_interference = (conf_key, interference)
+
+            fig, ax = plt.subplots()
+
+            ax.plot(write_baseline_iops, write_baseline_lat, markersize = 4, marker = '>', label="  0% reset")
+            ax.plot(write_iops, write_lat, markersize = 4, marker = '*', label=" 50% reset")
+
+            fig.tight_layout()
+            ax.grid(which='major', linestyle='dashed', linewidth='1')
+            ax.set_axisbelow(True)
+            # ax.legend(loc='best', handles=handles)
+            ax.legend(loc='best')
+            ax.set_ylim(bottom=0, top=140)
+            ax.set_xlim(left=0)
+            ax.set_ylabel("p95 write Latency (usec)")
+            ax.set_xlabel("Total IOPS (x1000)")
+            plt.savefig(f"{file_path}/figures/loaded_write_latency-{conf_key}.pdf", bbox_inches="tight")
+            plt.savefig(f"{file_path}/figures/loaded_write_latency-{conf_key}.png", bbox_inches="tight")
+            plt.clf()
+
+    print(f"Lowest Interference {lowest_interference[0]} Interference RMS 0% - 50% {lowest_interference[1]}")
